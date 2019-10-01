874975444f65f213fac0d8fa12b2d7673044f497
Alex Leybourne <alexleybourne1@gmail.com>
2019-10-01 10:53:01 +1000

    Added arguments

--- login/app/controllers/credentials_controller.rb
+++ login/app/controllers/credentials_controller.rb
@@ -2,6 +2,15 @@ class CredentialsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def login
-        render json: params
+        @users = [
+            { email: "jim@jimmail.com", password: "Jimmyjimmles"},
+            { email: "female@email.com", password: "girly"}
+        ]
+        user = @users.find{|user| user[:email] == params[:email]}
+        if user and user[:password] == params[:password]
+            render plain: "Logged in as #{user[:email]}!"
+        else
+            render plain: "Wrong email or password!"
+        end
     end
 end
